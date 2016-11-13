383c6aa8016cc3e743f9dc549b9b0ecb2883026c
Ailson Rocha Freire <ailson_freire@outlook.com>
2016-11-13 17:28:36 -0200

    v2
    Transição entra telas funcionando perfeitamente

--- app/src/main/java/com/example/ailson/testevariastelas/MainActivity.java
+++ app/src/main/java/com/example/ailson/testevariastelas/MainActivity.java
@@ -8,41 +8,66 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn1, btn2;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//        setContentView(R.layout.tela2);
 
         chamaTela1();
-
-        btn1 = (Button) findViewById(R.id.btn1);
-        btn2 = (Button) findViewById(R.id.btn2);
-
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                chamaTela2();
-            }
-        });
-
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                chamaTela1();
-            }
-        });
     }
 
+        public void chamaTela1(){
+            setContentView(R.layout.activity_main);
 
-           public void chamaTela2() {
-               setContentView(R.layout.tela2);
-           }
+           Button btn1 = (Button) findViewById(R.id.btn1);
 
-            public void chamaTela1() {
-                setContentView(R.layout.activity_main);
-            }
+           btn1.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View v) {
+                   chamaTela2();
+               }
+           });
 
+        }
+
+        public void chamaTela2(){
+            setContentView(R.layout.tela2);
+
+           Button btn2 = (Button) findViewById(R.id.btn2);
+           Button btn3 = (Button) findViewById(R.id.btn3);
+
+           btn2.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View v) {
+                   chamaTela1();
+               }
+           });
+
+           btn3.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View v) {
+                   chamaTela3();
+               }
+           });
+        }
+
+        public void chamaTela3(){
+            setContentView(R.layout.tela3);
+
+           Button btn4 = (Button) findViewById(R.id.btn4);
+           Button btn5 = (Button) findViewById(R.id.btn5);
+
+           btn4.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View v) {
+                   chamaTela2();
+               }
+           });
+
+           btn5.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View v) {
+                   chamaTela1();
+               }
+           });
+        }
 }
